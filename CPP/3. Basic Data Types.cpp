541ade52c34d4e8526e58a66b63e87efbde91faf
Siddharth Maratha <80692281+siddharthmaratha@users.noreply.github.com>
2021-06-24 00:04:36 +0530

    Add files via upload

---
+++ CPP/3. Basic Data Types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;
+    long k;
+    float b;
+    char ch;
+    double d;
+    scanf("%d %ld %c %f %lf", &i, &k, &ch, &b, &d);
+    printf("%d\n", i);
+    printf("%ld\n", k);
+    printf("%c\n", ch);
+    printf("%f\n",b);
+    printf("%lf\n",d);
+    return 0;
+}
+
