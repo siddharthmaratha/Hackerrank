0851b3c23887666eeccdf492cc99e1de8d68ab53
Siddharth Maratha <80692281+siddharthmaratha@users.noreply.github.com>
2021-06-27 01:12:51 +0530

    Add files via upload

---
+++ CPP/9.Variable Sized Arrays.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{ 
+    int w;
+    int x;
+    cin >> w >> x;
+    vector<int> a[w];
+    for(int i = 0; i < w; i++){
+        int y;
+        cin >> y;
+        int o;
+        for(int j = 0; j < y; j++){
+            cin >> o;
+            a[i].push_back(o);
+        }
+    }
+    
+    int u, v;
+    for(int k = 1; k <= x; k++){
+        cin >> u >> v;
+        cout << a[u][v] << endl;
+    }
+    return 0;
+}
